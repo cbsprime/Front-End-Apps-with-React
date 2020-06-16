1fe1b6e935ccfbac6141277a3c0182f91877d52d
Georgi Kardzhaliyski <gkardzhaliysky@gmail.com>
2020-06-16 14:05:37 -0400

    Create Functions.jsx

---
+++ Functions.jsx
@@ -0,0 +1,14 @@
+// In programming, we often use code to perform a specific task multiple times. Instead of rewriting the same code, we can group a block of code together 
+// and associate it with one task, then we can reuse that block of code whenever we need to perform the task again. We achieve this by creating a function.
+// A function is a reusable block of code that groups together a sequence of statements to perform a specific task.
+
+function getReminder() {
+    console.log('Water the plants');
+  }
+  
+  function greetInSpanish(){
+    console.log('Buenas Tardes.');
+  }
+    
+  // To call a function in your code, you type the function name followed by parentheses.
+
