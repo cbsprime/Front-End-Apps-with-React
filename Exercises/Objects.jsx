ceb3675c7a5fb4184925ee9334bd9359afc6ec54
Georgi Kardzhaliyski <gkardzhaliysky@gmail.com>
2020-08-06 21:39:12 -0400

    Update Objects.jsx

--- Exercises/Objects.jsx
+++ Exercises/Objects.jsx
@@ -96,5 +96,89 @@ let spaceship = {
   
   console.log(spaceship['Active Mission']);
 
+// Objects are mutable meaning we can update them after we create them.
+// We can use either dot notation, ., or bracket notation, [], and the assignment operator, = to add new key-value pairs to an object or change an existing property.
+
+// Although we can’t reassign an object declared with const, we can still mutate it, meaning we can add new properties and change the properties that are there.
+
+const spaceship = {type: 'shuttle'};
+spaceship = {type: 'alien'}; // TypeError: Assignment to constant variable.
+spaceship.type = 'alien'; // Changes the value of the type property
+spaceship.speed = 'Mach 5'; // Creates a new key of 'speed' with a value of 'Mach 5'
+
+// You can delete a property from an object with the delete operator.
+
+const spaceship = {
+    'Fuel Type': 'Turbo Fuel',
+    homePlanet: 'Earth',
+    mission: 'Explore the universe' 
+  };
+  
+  //delete objectName['Property Name'];
+// delete objectName.propName;
+
+  delete spaceship.mission;  // Removes the mission property
+
+// Re-assign property
+objectName['Property Name'] = 'New Property Value';
+objectName.propName = 'New Prop Value';
+
+let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth',
+    color: 'silver',
+    'Secret Mission' : 'Discover life outside of Earth.'
+  };
+  
+// Write your code below
+  spaceship.color = 'glorious gold';
+  spaceship.numEngines = 4;
+  
+  delete spaceship ['Secret Mission'];
+
+// When the data stored on an object is a function we call that a method. A property is what an object has, while a method is what an object does.
+//  For example console is a global javascript object and .log() is a method on that object. Math is also a global javascript object and .floor() is a method on it.
+
+// With the new method syntax introduced in ES6 we can omit the colon and the function keyword.
+const alienShip = {
+    invade () { 
+      console.log('Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon.')
+    }
+  };
+
+  // Object methods are invoked by appending the object’s name with the dot operator followed by the method name and parentheses:
+
+  alienShip.invade(); // Prints 'Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon.'
+
+  // 
+
+  let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+// Write your code below
+
+let alienShip = {
+  retreat() {
+    console.log(retreatMessage)
+  },
+  takeOff() {
+    console.log('Spim... Borp... Glix... Blastoff!')
+  }
+};
+
+alienShip.retreat();
+
+alienShip.takeOff();
+
+// Don’t forget to separate your methods with commas just as you would any other key-value pairs:
+let objectName = {
+    methodName() {
+      console.log('The methodName method was invoked!')
+    }, 
+    secondMethodName() {
+       console.log('The secondMethodName method was invoked!')
+  }
+  };
+
+
 
 
